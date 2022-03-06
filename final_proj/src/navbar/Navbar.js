7bf40a412466ca37b821f0d7060497d47c8df448
HNOONa-0 <59091536+HNOONa-0@users.noreply.github.com>
2022-03-06 15:01:54 +0200

    updating links

--- final_proj/src/navbar/Navbar.js
+++ final_proj/src/navbar/Navbar.js
@@ -2,17 +2,18 @@ import React from "react";
 import "./NavbarStyles.css";
 // changes
 // name change to Navbar from NavBar
+let url = "https://hnoona-0.github.io/React_Graph_Project/";
 const Navbar = () => {
   return (
     <div className="wrapper-nav-bar">
       <nav className="nav-bar">
         <div className="logo">
           {/* <h2>Graph</h2> */}
-          <a href="http://localhost:3000/">Graph</a>
+          <a href={url}>Graph</a>
         </div>
         <ul className="nav-links">
           <li>
-            <a href="http://localhost:3000/Guide">Guide</a>
+            <a href={url + "Guide"}>Guide</a>
           </li>
           {/* <li>
             <a href="http://localhost:3000/About">About</a>
